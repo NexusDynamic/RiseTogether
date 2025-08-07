3ead18ddd0bfb4b75b3bbbfb2092d7a8f5f007c6
zeyus <dev@zeyus.com>
2025-08-07 15:22:33 +0200

    migrate to easy_shared_preferences package.

--- lib/src/settings/physics.dart
+++
@@ -1,8 +0,0 @@
-import 'setting.dart';
-import 'settings_group.dart';
-
-/// Game settings group containing all physics configuration.
-final physicsSettings = SettingsGroup(
-  key: 'physics',
-  items: [DoubleSetting(key: 'gravity', defaultValue: 9.81)],
-);
